4fcf5cd425757fdd3e023331b9ef7bb656de2a6b
kene-onwumbiko <158063431+kene-onwumbiko@users.noreply.github.com>
2024-07-20 18:39:41 +0100

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -96,98 +96,11 @@ sep_2020_undergrad_computing = sep_2020_undergrad[sep_2020_undergrad["School / D
 
 
 
-# Create a dictionary to store the continent names.
-# This dictionary is used to get the map of each continent.
-continents = {"World": "world",
-              "Asia": "asia",
-              "Africa": "africa",
-              "Europe": "europe",
-              "North America": "north america",
-              "South America": "south america"}
 
-# Labels for policies.
-# These lists are used to label the y axis of the policy line graph.
-school_closing = ["No measures", "Recommend closing", "Require localised closing", "Require all closing"]
-stay_at_home = ["No measures", "Recommend stay home", "Stay home with some exception", "Stay home with minimum exception"]
 
-# Define a function to create the bubble map.
-def create_bubble_map(map_covid_data, scope, data_input):
-    fig = px.scatter_geo(map_covid_data,
-                         locations = "CountryCode",
-                         color = "Continent_Name",
-                         hover_name = "CountryName",
-                         size = data_input,
-                         size_max = 50,
-                         animation_frame = "Date",
-                         projection = "equirectangular",
-                         opacity = 0.6,
-                         title = f"COVID-19 {data_input} in {scope}",
-                         color_discrete_sequence = px.colors.qualitative.T10)
-    
-    # Update geos for the scope.
-    fig.update_geos(projection_type = "equirectangular", 
-                    scope = continents[scope], 
-                    showcountries = True)
-    return fig
 
-# Define a function to create the choropleth map.
-def create_choropleth_map(map_covid_data, scope, policy):
-    fig = px.choropleth(map_covid_data,
-                        locations = "CountryCode",
-                        color = policy,
-                        hover_name = "CountryName",
-                        animation_frame = "Date",
-                        projection = "equirectangular",
-                        title = f"{policy} policy in {scope}",
-                        color_continuous_scale = px.colors.sequential.Plasma)
-    
-    # Update geos for the scope.
-    fig.update_geos(projection_type = "equirectangular", 
-                    scope = continents[scope], 
-                    showcountries = True)
-    return fig
 
-# Define a function to create the line graph.
-def create_line_graph(map_covid_data, data_input, policy):
-    
-    # Filter the top five countries as of 20th May 2020 for confirmed cases.
-    top_5_countries = map_covid_data[map_covid_data["Date"] == pd.Timestamp("2020-05-20").date()].drop_duplicates(subset = map_covid_data.columns[0:8]).nlargest(5, "ConfirmedCases")["CountryName"].unique()
-    filtered_top_5_countries = map_covid_data[map_covid_data["CountryName"].isin(top_5_countries)]
 
-    if data_input:
-        
-        # Line graph for data input
-        fig = px.line(filtered_top_5_countries, 
-                      x = "Date", 
-                      y = data_input, 
-                      color = "CountryName", 
-                      title = f"COVID-19 {data_input} in the top five countries")
-    else:
-        
-        # Line graph for policy
-        fig = px.line(filtered_top_5_countries,
-                      x = "Date", 
-                      y = policy, 
-                      color = "CountryName", 
-                      title = f"{policy} policy in the top five countries")
-        
-        # Set ticktext for each policy.
-        if policy == "School closing":
-            ticktext = school_closing
-        elif policy == "Stay at home requirements":
-            ticktext = stay_at_home
-        else:
-            ticktext = []
-        
-        # Update layout for the line gragh
-        fig.update_layout(yaxis_title = f"{policy} policy level",
-                          yaxis = dict(
-                              tickmode = "array",
-                              tickvals = [0, 1, 2, 3],
-                              ticktext = ticktext,
-                              tickfont = dict(size = 10))
-                          )
-    return fig
 
 
 
