db477dc6a8a0e6792f55aaed6f812a227195ed89
kene-onwumbiko <158063431+kene-onwumbiko@users.noreply.github.com>
2024-11-11 21:48:01 +0000

    Update Final_Project_2.py

--- Final_Project_2.py
+++ Final_Project_2.py
@@ -22,9 +22,17 @@ final_registrations = pd.read_csv(r"C:\Users\keneo\OneDrive\Desktop\Github Proje
 dates_to_drop = ["Sep 2023", "Sep 2018", "Sep 2017", "Jan 2023", "Jan 2018", "Jan 2017"]
 final_records = final_records[~final_records["Date"].isin(dates_to_drop)]
 
+final_registrations = final_registrations.drop(columns = ["Month", "Year", "Main Level"])
 
+# Group by "Campus", "Group", "School / Department", "Level", and "Category"
+# Then sum the values for "Number of Acceptances" and "Number of Applications"
+final_summary = final_records.groupby(
+    ["Campus", "Group", "School / Department", "Level", "Date"], as_index = False
+)[["Number of Applications", "Number of Acceptances"]].sum().sort_values(by = ["Group", "Date"], 
+                                                                         ascending = False)
 
-
+on_values = ["Campus", "Group", "School / Department", "Level", "Date"]                                                                         
+final_summary = final_summary.merge(final_registrations, on = on_values, how = "outer")
 
 
 
